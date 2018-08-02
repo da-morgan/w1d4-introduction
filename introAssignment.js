5813a6dd57ee7d58603df7aae68b9df3d0e543f4
David Morgan <david.alexandermorgan@gmail.com>
2018-08-02 18:55:54 +0000

    Refactored to include a for each statement

--- introAssignment.js
+++ introAssignment.js
@@ -1,13 +1,15 @@
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(function(name) {
+    if (name === "Waldo") {
+      found(arr.indexOf(name));   // execute callback
     }
-  }
+  });
 }
 
 function actionWhenFound(index) {
   console.log("Found Waldo at index " + index + "!");
 }
+
+
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
